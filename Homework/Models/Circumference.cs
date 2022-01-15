9d1463c35e2b2e4f5028e0e4f8b00779adaa1694
Ольга Черепанова <chernichka.ekb@gmail.com>
2022-01-15 16:39:10 +0500

    Добавьте файлы проекта.

---
+++ Homework/Models/Circumference.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework.Models
+{
+    static class Circumference
+    {
+        public static double Add(double radius) => 2 * radius * Math.PI;
+    }
+}
---
+++ Homework/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,63 @@
+﻿using Homework.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Homework.ViewModels
+{
+    class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private double r;
+        public double R
+        {
+            get => r;
+            set
+            {
+                r = value;
+                OnPropertyChanged();
+            }
+        }
+
+         void OnPropertyChanged([CallerMemberName] string PropertyName=null)
+        {
+            PropertyChanged?.Invoke(this,new PropertyChangedEventArgs(PropertyName));
+        }
+
+        private double l;
+        public double L
+        {
+            get => l;
+            set
+            {
+                l = value;
+                OnPropertyChanged();
+            }
+        }
+        public ICommand AddCommand { get; }
+
+        private void OnAddCommandExecute(object p)
+        {
+            L = Circumference.Add(R);
+        }
+        private bool CanAddCommandExecute(object p)
+        {
+            if (R >= 0)
+            {
+                return true;
+            }
+            else
+                return
+                    false;
+        }
+        public MainWindowViewModel()
+        {
+            AddCommand = new RelayCommand(OnAddCommandExecute, CanAddCommandExecute);
+        }
+    }
+}
